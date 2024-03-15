6d6ab27b330b57af7f40a227a5f08886009aa87e
Adam Škoda <adamxskoda@gmail.com>
2024-03-15 14:04:34 +0100

    Add new parameter to the convertDateLocale filter

--- backend/src/lib/report-generator.js
+++ backend/src/lib/report-generator.js
@@ -207,6 +207,10 @@ expressions.filters.convertDateLocale = function(input, locale, style) {
         if (style === "full")
             options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}
 
+	if (style === "single")
+       	    var options = { year: 'numeric', month: 'numeric', day: 'numeric'}
+
+
         return date.toLocaleDateString(locale, options)
 
     }
